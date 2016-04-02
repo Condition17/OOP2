0253c0eeff6c598f24552ab40a9385543f126cd6
Conache Cristian <c.conache@gmail.com>
2016-04-02 23:21:28 +0300

    Little changes

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 {
     tree* t = new tree("green",2,1,2,22);
     tree* g = new tree("yellow",3,1,21,34);
-
+    tree* h = new tree("black",3,15,21,34);
    // Resource_manager::instance() -> write();
     // cout<<t -> get_number()<<"\n";\
    // Resource_manager::instance() -> show();
@@ -17,8 +17,11 @@ int main()
     delete t;
     cout<<"\n---\n";
 //    cout<<t -> get_number()<<"\n";
+Resource_manager::instance() -> show();
     delete g;
  //   Resource_manager::instance() -> show();
-
+ Resource_manager::instance() -> show()
+    delete h;
+Resource_manager::instance() -> show();
     return 0;
 }
